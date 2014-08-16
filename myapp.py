d432f74cb1f9df8651b37d0bd0e4989b648e6f7f
Tom Keene <tom@theanthillsocial.co.uk>
2014-08-16 11:06:34 +0100

    Add WWWWWW to the test.py file 2 2 2 2

--- myapp.py
+++ myapp.py
@@ -1,7 +1,7 @@
 # A new project
 print('Hello World!')
 print('Hello World!')
-print('Hello World!')
+print('Hello WWWWWWWWWWWWWWWW')
 print('Hello World!')
 print('Hello World!')
 print('Hello World!')
