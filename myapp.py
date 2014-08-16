3d801b9e0a1e5b780ca5d8640413770bca8e3338
Tom Keene <tom@theanthillsocial.co.uk>
2014-08-16 11:18:22 +0100

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,8 +1,3 @@
 # A new project
 print('Hello World!')
-print('Hello World!')
-print('Hello WWWWWWWWWWWWWWWW')
-print('Hello World!YYYYYYYYYYYYTYYYYYYYY)
-print('Hello World!')
-print('Hello World!')
-print('Hello World!')
+
