90053e3aa940f8eeae2dab674d8c104d72a3875e
Tom Keene <tom@theanthillsocial.co.uk>
2014-08-16 11:08:45 +0100

    Script not working

--- myapp.py
+++ myapp.py
@@ -2,7 +2,7 @@
 print('Hello World!')
 print('Hello World!')
 print('Hello WWWWWWWWWWWWWWWW')
-print('Hello World!')
+print('Hello World!YYYYYYYYYYYYTYYYYYYYY)
 print('Hello World!')
 print('Hello World!')
 print('Hello World!')
